49a41e9dae15145c6e14c041985d7665df76a690
Ameen Khan <mak_24x7@hotmail.co.uk>
2024-04-01 13:43:20 -0400

    update products model

--- public/products/table_products.sql
+++ public/products/table_products.sql
@@ -1,11 +1,12 @@
 -- Product Table
+DROP TABLE public.products CASCADE ;
 CREATE TABLE IF NOT EXISTS public.products
 (
     product_id         SERIAL PRIMARY KEY,
     name               VARCHAR(100)   NOT NULL,
     description        TEXT,
     price              DECIMAL(10, 2) NOT NULL,
-    quantity_available INT            NOT NULL,
+    quantity_unit      VARCHAR(100)   NOT NULL, -- lbs, pieces, etc,
     is_available       BOOLEAN   DEFAULT true, -- Indicates if the product is currently being sold
     image_url          VARCHAR(255), -- Add this column to store the image URL
     created_at         TIMESTAMP DEFAULT CURRENT_TIMESTAMP
